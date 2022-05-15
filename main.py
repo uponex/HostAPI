7b38131d1b19ccc3f5557a4b4d4693d66bc616c8
uponex <34040615+uponex@users.noreply.github.com>
2022-05-15 10:03:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+from PIL import Image, ImageFilter
+from fastapi import FastAPI, Depends, File, UploadFile
+from starlette.responses import StreamingResponse
+from io import BytesIO
+import uvicorn
+
+app = FastAPI()
+# @app.get("/")
+# async def start():
+#     return
+
+@app.post("/CONTOUR")
+async def image_filter(img: UploadFile = File(...)):
+    original_image = Image.open(img.file)
+    original_image = original_image.filter(ImageFilter.CONTOUR)
+    #CONTOUR, EMBOSS, FIND_EDGES
+    filtered_image = BytesIO()
+    original_image.save(filtered_image, "JPEG")
+    filtered_image.seek(0)
+
+    return StreamingResponse(filtered_image, media_type="image/jpg")
+
+@app.post("/EMBOSS")
+async def image_filter(img: UploadFile = File(...)):
+    original_image = Image.open(img.file)
+    original_image = original_image.filter(ImageFilter.EMBOSS)
+    #CONTOUR, EMBOSS, FIND_EDGES
+    filtered_image = BytesIO()
+    original_image.save(filtered_image, "JPEG")
+    filtered_image.seek(0)
+
+    return StreamingResponse(filtered_image, media_type="image/jpg")
+
+@app.post("/EDGES")
+async def image_filter(img: UploadFile = File(...)):
+    original_image = Image.open(img.file)
+    original_image = original_image.filter(ImageFilter.FIND_EDGES)
+    #CONTOUR, EMBOSS, FIND_EDGES
+    filtered_image = BytesIO()
+    original_image.save(filtered_image, "JPEG")
+    filtered_image.seek(0)
+
+    return StreamingResponse(filtered_image, media_type="image/jpg")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=5000, log_level="info")
